03cc8c24fb3dfce3db7aaa06a2465253d6778ddf
akshaypaturkar07 <akshaydp07@gmail.com>
2020-08-16 13:11:53 +0530

    enabled swagger

--- src/main/java/com/sapient/controller/StandingController.java
+++ src/main/java/com/sapient/controller/StandingController.java
@@ -1,10 +1,8 @@
 package com.sapient.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
--- src/main/java/com/sapient/footballdemo/FootballdemoApplication.java
+++ src/main/java/com/sapient/footballdemo/FootballdemoApplication.java
@@ -1,5 +1,10 @@
 package com.sapient.footballdemo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,24 +13,25 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class FootballdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FootballdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FootballdemoApplication.class, args);
+    }
 
-	/*@Bean
-	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
-		return new OpenAPI().info(new Info()
-				.title("Order Detail API")
-				.version(appVersion)
-				.description(appDesciption)
-				.termsOfService("http://swagger.io/terms/")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+        return new OpenAPI().info(new Info()
+                .title("Order Detail API")
+                .version(appVersion)
+                .description(appDesciption)
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
 
-	}*/
-	@Bean
-	//@LoadBalanced
-	RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
+    }
+
+    @Bean
+        //@LoadBalanced
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
